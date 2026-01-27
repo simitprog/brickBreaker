76c2aacfa99c40fad0b20b0b2e123766272d6bac
user17403 <t1m4x.08@gmail.com>
2026-01-27 22:19:00 +0100

    file che contiene la classe giocatore grafico

---
+++ GiocatoreGrafico.java
@@ -0,0 +1,46 @@
+import java.awt.Color;
+import java.awt.Graphics;
+
+/**
+ * @class GiocatoreGrafico
+ * @brief Gestisce la parte grafica della racchetta (il giocatore) utilizzando la giocatoreLogico
+ */
+public class GiocatoreGrafico {
+    private giocatoreLogico giocatore;
+    private Color colore;
+
+    /**
+     * @brief costruttore
+     * @param giocatore giocatoreLogico associato
+     * @param colore colore della racchetta
+     */
+    public GiocatoreGrafico(giocatoreLogico giocatore, Color colore) {
+        this.giocatore = giocatore;
+        this.colore = colore;
+    }
+
+    /**
+     * @brief disegna il giocatore
+     * @param g oggetto su cui disegnare
+     */
+    public void disegna(Graphics g){
+        int x=(int)giocatore.getPosizione().getX();
+        int y=(int)giocatore.getPosizione().getY();
+        int larghezza=(int)giocatore.getLarghezza();
+        int altezza=(int)giocatore.getAltezza();
+
+        g.setColor(colore);
+        g.fillRect(x,y,larghezza,altezza);
+
+        g.setColor(Color.BLACK);
+        g.drawRect(x,y,larghezza,altezza);
+    }
+
+    /**
+     * @brief setter per il colore
+     * @param colore nuovo colore
+     */
+    public void setColore(Color colore){
+        this.colore=colore;
+    }
+}
