dd52156e00442f66a96e0b433d1864da7082d692
JEANMONNET\ratti_francesco08 <fra.rat.11@gmail.com>
2026-01-30 10:49:18 +0100

    Fatta classe bloccografico

---
+++ BonusGrafico.java
@@ -0,0 +1,62 @@
+import javax.swing.*;
+import java.awt.*;
+import java.awt.event.*;
+import java.util.ArrayList;
+import java.util.List;
+
+
+public class BonusGrafico {
+    private BonusLogico bonus;
+    private Image immagine;
+    private MyPanel pannello;
+
+    public BonusGrafico(BonusLogico bonus, Image immagine, MyPanel pannello) {
+        this.bonus = bonus;
+        this.immagine = immagine;
+        this.pannello = pannello;
+    }
+
+
+    public BonusLogico getBonus() {
+        return bonus;
+    }
+
+
+    public void setBonus(BonusLogico bonus) {
+        this.bonus = bonus;
+    }
+
+
+    public Image getImmagine() {
+        return immagine;
+    }
+
+
+    public void setImmagine(Image immagine) {
+        this.immagine = immagine;
+    }
+
+
+
+
+    public void disegna (Graphics g){
+        int x = (int)bonus.getCoordinate().getX();
+        int y=(int)bonus.getCoordinate().getY();
+        
+        g.drawImage(immagine,x,y,30,30,pannello);
+    
+    }
+
+
+    
+
+
+
+
+
+
+
+
+
+
+}
