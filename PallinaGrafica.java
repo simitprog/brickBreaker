f3ed22c54c1c44c8dd46f73a7ee299239a1a6468
alexandromatteotimis <alexandromatteo.timis@gmail.com>
2026-02-03 11:37:28 +0100

    tolti import non usati

--- PallinaGrafica.java
+++ PallinaGrafica.java
@@ -11,32 +11,32 @@ public class PallinaGrafica {
 
     /**
      * @brief costruttore
-     * @param palla pallinaLogica associata
+     * @param palla  pallinaLogica associata
      * @param colore colore della pallina
      */
-    public PallinaGrafica(PallinaLogica palla, Color colore){
-        this.pallina=palla;
-        this.colore=colore;
+    public PallinaGrafica(PallinaLogica palla, Color colore) {
+        this.pallina = palla;
+        this.colore = colore;
     }
 
     /**
      * @brief disegna la pallina
      * @param g oggetto su cui disegnare
      */
-    public void disegna(Graphics g){
-        int x=(int)pallina.getPosizione().getX();
-        int y=(int)pallina.getPosizione().getY();
-        int diametro=(int)pallina.getRaggio()*2;
+    public void disegna(Graphics g) {
+        int x = (int) pallina.getPosizione().getX();
+        int y = (int) pallina.getPosizione().getY();
+        int diametro = (int) pallina.getRaggio() * 2;
 
         g.setColor(colore);
         g.fillOval(x, y, diametro, diametro);
 
         g.setColor(Color.BLACK);
-        g.drawOval(x,y,diametro,diametro);
+        g.drawOval(x, y, diametro, diametro);
     }
 
-    public void setColore(Color colore){
-        this.colore=colore;
+    public void setColore(Color colore) {
+        this.colore = colore;
     }
 
 }
